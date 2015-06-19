d6e8e39fde9e0b52e992e841fa69032cf01b5706
Frédéric Fauberteau <frederic@fauberteau.org>
2015-06-19 12:14:48 +0200

    Add usage method

--- src/main/java/grts/main/Main.java
+++ src/main/java/grts/main/Main.java
@@ -1,3 +1,29 @@
+/* Copyright (c) 2015, Tristan Fautrel
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package grts.main;
 
 import grts.core.architecture.Architecture;
@@ -13,13 +39,21 @@ import grts.core.taskset.TaskSet;
 import grts.core.taskset.TaskSetFactory;
 import grts.logger.EventLogger;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 
+/**
+ * @author Tristan Fautrel
+ */
 public class Main {
+
+    private final static int EXIT_FAILURE = 1;
 
     public static void main(String[] args) {
 //        AbstractRecurrentTask t1 = new PeriodicTask(7, 2, 7, 0, "t1");
@@ -84,12 +118,17 @@ public class Main {
 //        long timer = ts.getHyperPeriod();
 //        simulator.simulate(timer);
 
+        if (args.length < 2) {
+            usage();
+            System.exit(EXIT_FAILURE);
+        }
+
         InputStream inputStreamTasks;
+        InputStream inputStreamArchitecture;
         SimulatorJacksonParser parser;
         try {
-            inputStreamTasks = Files.newInputStream(Paths.get("PeriodicTaskSet1.json"));
-//            InputStream inputStreamArchitecture = Files.newInputStream(Paths.get("ArchitectureTwoProcessors.json"));
-            InputStream inputStreamArchitecture = Files.newInputStream(Paths.get("ArchitectureMonoProcessor.json"));
+            inputStreamTasks = Files.newInputStream(Paths.get(args[0]));
+            inputStreamArchitecture = Files.newInputStream(Paths.get(args[1]));
             parser = new SimulatorJacksonParser(inputStreamTasks, inputStreamArchitecture);
         } catch (IOException e) {
             e.printStackTrace();
@@ -133,4 +172,20 @@ public class Main {
         simulator.simulate(timer);
         logger.writeJson();
     }
+
+    private static void usage() {
+        Path current = Paths.get(System.getProperty("user.dir"));
+        Path classPath = Paths.get(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        Path target = current.relativize(classPath);
+        StringBuilder sb = new StringBuilder();
+        sb.append("java").append(' ');
+        if (classPath.toString().endsWith(".jar")) {
+            sb.append("-jar").append(' ').append(target);
+        } else {
+            sb.append("-cp").append(' ').append(target).append(' ').append(Main.class.getCanonicalName());
+        }
+        sb.append(' ').append("TASKSET.json").append(' ').append("ARCHITECTURE.json");
+        System.out.println(sb.toString());
+    }
+
 }
